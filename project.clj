0e12d462a9261711dac012e95122dfc193e678c4
Chris Pickard <chris_pickard@sra.com>
2015-07-29 10:04:58 -0400

    fixing the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject good-old-days "0.1.0-SNAPSHOT"
   :description "A thoroughly useful library that brings back the days of yore"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ChrisPickard/good-old-days"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
